a1810049b36c861adb6d79f199652561a228f17b
diurivj <diurivj@icloud.com>
2018-07-15 16:00:16 -0500

    nothing yet

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const hbs          = require('hbs');
 const mongoose     = require('mongoose');
 const logger       = require('morgan');
 const path         = require('path');
-
+const cors         = require('cors');
 
 mongoose.Promise = Promise;
 mongoose
@@ -31,28 +31,22 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 
 // Express View engine setup
-
 app.use(require('node-sass-middleware')({
   src:  path.join(__dirname, 'public'),
   dest: path.join(__dirname, 'public'),
   sourceMap: true
 }));
       
-
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(favicon(path.join(__dirname, 'public', 'images', 'favicon.ico')));
 
-
-
-// default value for title local
+// Default value for title local
 app.locals.title = 'Express - Generated with IronGenerator';
 
-
-
+//Routes
 const index = require('./routes/index');
 app.use('/', index);
-
 
 module.exports = app;
